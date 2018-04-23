e32e8b50d14abea7aeef0594aa4888097a3b3d20
Oleg <o.staselovich@yandex.com>
2018-04-23 23:58:50 +0300

    task4

--- homeworks/task_queue/server.py
+++ homeworks/task_queue/server.py
@@ -1,6 +1,184 @@
+import socket
+import sys
+import shelve
+import datetime
+from collections import OrderedDict
 
-def run():
-    pass
+
+class Queues:
+    def __init__(self, timeout):
+        self._queues = dict()
+        self._filename = 'log'
+        self._id_counter = 0
+        self._timedelta = datetime.timedelta(minutes=timeout)
+        self._write_err_msg = "something was wrong when writing to file..."
+
+    def add_command(self, queue_name, length, data):
+        if queue_name not in self._queues:
+            self._queues[queue_name] = OrderedDict()
+
+        task_id = str(self._id_counter)
+        running = False
+        time_get = None
+        task_params = [length, data, running, time_get]
+        self._queues[queue_name][task_id] = task_params
+        self._id_counter += 1
+        if not self.save_changes_to_file():
+            raise IOError(self._write_err_msg)
+
+        return task_id
+
+    def get_command(self, queue_name):
+        if queue_name not in self._queues:
+            return None
+
+        time_now = datetime.datetime.now()
+        for task_id, task_params in self._queues[queue_name].items():
+            if not task_params[2]:
+                task_params[2] = True
+                task_params[3] = time_now
+                if not self.save_changes_to_file():
+                    raise IOError(self._write_err_msg)
+
+                return ' '.join([task_id, task_params[0], task_params[1]])
+
+        return None
+
+    def ack_command(self, queue_name, task_id):
+        if queue_name not in self._queues:
+            raise ValueError("{} doesn't exist".format(queue_name))
+        if self.in_command(queue_name, task_id) == 'NO':
+            status = "NO"
+        else:
+            self.check_timeout(queue_name, task_id)
+            if not self._queues[queue_name][task_id][2]:
+                status = "NO"
+            else:
+                self._queues[queue_name].pop(task_id)
+                status = "YES"
+
+        if not self.save_changes_to_file():
+            raise IOError(self._write_err_msg)
+        return status
+
+    def in_command(self, queue_name, task_id):
+        if queue_name not in self._queues:
+            raise ValueError("{} doesn't exist".format(queue_name))
+        if task_id in self._queues[queue_name].keys():
+            status = "YES"
+        else:
+            status = "NO"
+
+        if not self.save_changes_to_file():
+            raise IOError(self._write_err_msg)
+        return status
+
+    def check_timeout(self, queue_name, task_id):
+        task_time_delta = datetime.datetime.now() - self._queues[queue_name][task_id][3]
+        if task_time_delta > self._timedelta:
+            self._queues[queue_name].pop(task_id)
+
+    def save_changes_to_file(self):
+        try:
+            with shelve.open(self._filename, 'n') as log_file:
+                    for queue in self._queues:
+                        log_file[queue] = self._queues[queue]
+        except IOError:
+            return False
+
+        return True
+
+    def read_file(self):
+        try:
+            with shelve.open(self._filename, 'c') as log_file:
+                    for queue in log_file.keys():
+                        self._queues[queue] = log_file[queue]
+        except IOError:
+            return False
+
+        return True
+
+
+def run(port=8080, timeout=5, ip_addr='0.0.0.0'):
+    max_command_length = 5000000
+    queues = Queues(timeout)
+    queues.read_file()
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    connection.bind((ip_addr, port))
+    connection.listen(10)
+    while True:
+        current_connection, address = connection.accept()
+        command = current_connection.recv(max_command_length)
+        response = command_processing(command, queues)
+        send_and_close(current_connection, response)
+
+
+def command_processing(command, queues):
+    command = command.decode('utf-8')
+    if not check_command(command):
+        return None
+
+    commands = {'ADD': queues.add_command, 'GET': queues.get_command,
+                'ACK': queues.ack_command, 'IN': queues.in_command}
+    parsed_command = command.split()
+    method_name = parsed_command[0]
+    params = parsed_command[1:]
+    response = commands[method_name](*params)
+    b_response = response.encode()
+    return b_response
+
+
+def check_command(command):
+    params = command.split()
+    try:
+        if params[0] not in ['GET', 'ADD', 'ACK', 'IN']:
+            return False
+        if params[0] == 'GET' and len(params) != 2:
+            return False
+        if params[0] == 'IN' and len(params) != 3:
+            return False
+        if params[0] == 'ACK' and len(params) != 3:
+            return False
+        if params[0] == 'ADD' and len(params) != 4:
+            return False
+        return True
+    except (ValueError, IndexError):
+        return False
+
+
+def send_and_close(current_connection, data):
+    current_connection.send(data)
+    current_connection.shutdown(1)
+    current_connection.close()
+
+
+def parse_args(_args):
+    try:
+        if not 0 <= int(_args[0]) <= 65535:
+            raise ValueError
+        _args[0] = int(_args[0])
+
+        if len(_args) > 1:
+            if not 1 <= int(_args[1]) <= 10000:
+                raise ValueError
+            _args[1] = int(_args[1])
+
+        if len(_args) > 2:
+            ip_addr = _args[2].split('.')
+            if len(ip_addr) != 4 or not all([0 <= int(i) <= 255 for i in ip_addr]):
+                raise ValueError
+
+        return _args
+    except ValueError:
+        print("Incorrect params\nUsage: python3 server.py [port] [timeout] [ip_address]")
+        return None
+
 
 if __name__ == '__main__':
-    run()
+    if len(sys.argv) < 2:
+        run()
+    else:
+        args = parse_args(sys.argv[1:])
+        if args:
+            run(*args)
--- homeworks/task_queue/tests/test_server.py
+++ homeworks/task_queue/tests/test_server.py
@@ -1,17 +1,75 @@
 from unittest import TestCase
-
+from server import Queues
+from collections import OrderedDict
 import time
 import socket
-
 import subprocess
+import datetime
 
-from server import run
+
+class QueuesTest(TestCase):
+    def setUp(self):
+        self.server = subprocess.Popen(['python3', 'server.py', '8080'])
+        time.sleep(0.5)
+        self.queues = Queues(5)
+
+    def tearDown(self):
+        self.server.terminate()
+        self.server.wait()
+
+    def test_init(self):
+        expected_results = (dict(), 'log', 0, datetime.timedelta(minutes=5))
+        real_results = (self.queues._queues, self.queues._filename,
+                        self.queues._id_counter, self.queues._timedelta)
+        self.assertEqual(expected_results, real_results)
+
+    def test_check_timeout(self):
+        test_time1 = datetime.datetime.now() - datetime.timedelta(minutes=4)
+        self.queues._queues[0] = OrderedDict()
+        self.queues._queues[0][0] = ['5', '12345', False, test_time1]
+        self.queues.check_timeout(0, 0)
+        self.assertEqual(len(self.queues._queues[0]), 1)
+
+        test_time2 = datetime.datetime.now() - datetime.timedelta(minutes=5)
+        self.queues._queues[0][0] = ['5', '12345', False, test_time2]
+        self.queues.check_timeout(0, 0)
+        self.assertEqual(len(self.queues._queues[0]), 0)
+
+    def test_add_command(self):
+        id1 = self.queues.add_command('ABC', '6', '123456')
+        id2 = self.queues.add_command('BCD', '7', '1234567')
+        id3 = self.queues.add_command('ABC', '5', '12345')
+        q1 = self.queues._queues['ABC']
+        q2 = self.queues._queues['BCD']
+        ids = [id1, id2, id3]
+        self.assertEqual(ids, ['0', '1', '2'])
+        self.assertEqual(q1, OrderedDict([('0', ['6', '123456', False, None]),
+                                          ('2', ['5', '12345', False, None])]))
+        self.assertEqual(q2, OrderedDict([('1', ['7', '1234567', False, None])]))
+
+    def test_in_command(self):
+        task_id1 = self.queues.add_command('ABC', '6', '123456')
+        task_id2 = self.queues.add_command('ABCD', '4', '1234')
+        self.assertEqual(self.queues.in_command('ABC', task_id1), "YES")
+        self.assertEqual(self.queues.in_command('ABCD', task_id1), "NO")
+        self.assertEqual(self.queues.in_command('ABC', task_id2), "NO")
+
+    def test_ack_command(self):
+        task_id = self.queues.add_command('ABC', '6', '123456')
+        self.queues._queues['ABC'][task_id][3] = datetime.datetime.now()
+        self.assertEqual(self.queues.ack_command('ABC', task_id), "NO")
+
+    def test_get_command(self):
+        task_id = self.queues.add_command('ABC', '6', '123456')
+        self.assertEqual(self.queues.get_command('ABC'), task_id + ' 6' + ' 123456')
+        self.assertEqual(self.queues.get_command('ABC'), None)
 
 
 class ServerBaseTest(TestCase):
     def setUp(self):
-        self.server = subprocess.Popen(['python', 'server.py'])
-        # даем серверу время на запуск
+        self.server = subprocess.Popen(['python3', 'server.py'])
+        self._ip_addr = '127.0.0.1'
+        self._port = 8080
         time.sleep(0.5)
 
     def tearDown(self):
@@ -20,9 +78,9 @@ class ServerBaseTest(TestCase):
 
     def send(self, command):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('127.0.0.1', 5555))
+        s.connect(('127.0.0.1', 8080))
         s.send(command)
-        data = s.recv(1000000)
+        data = s.recv(5000000)
         s.close()
         return data
 
@@ -49,6 +107,21 @@ class ServerBaseTest(TestCase):
 
         self.assertEqual(b'YES', self.send(b'ACK 1 ' + second_task_id))
         self.assertEqual(b'NO', self.send(b'ACK 1 ' + second_task_id))
+
+    def test_read_file(self):
+        task_id = self.send(b'ADD 1 5 12345')
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + task_id))
+        self.assertEqual(task_id + b' 5 12345', self.send(b'GET 1'))
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + task_id))
+
+        self.tearDown()
+        time.sleep(0.5)
+        self.server = subprocess.Popen(['python3', 'server.py'])
+        time.sleep(0.5)
+
+        self.assertEqual(b'YES', self.send(b'ACK 1 ' + task_id))
+        self.assertEqual(b'NO', self.send(b'ACK 1 ' + task_id))
+        self.assertEqual(b'NO', self.send(b'IN 1 ' + task_id))
 
 
 if __name__ == '__main__':
